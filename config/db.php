46eaaa76a7dae7ad74c820b0483ddaf47b33b3fd
pragmatic335 <pragmatik335@gmail.com>
2021-04-10 13:23:22 +0300

    'intro'

--- config/db.php
+++ config/db.php
@@ -6,11 +6,6 @@ return [
     'username' => 'postgres',
     'password' => '',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
 
 
